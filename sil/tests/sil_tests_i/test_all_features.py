74bf80dd46320547a4d9fd734f278051168c6943
Victor Tsvyk <viktor.tsvyk@globallogic.com>
2025-03-28 16:11:25 +0200

    remove ivan test

--- sil/tests/sil_tests_i/test_all_features.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-from pytest_bdd import scenarios
-from sil_tests_i.steps import *
-
-features_path = Path(__file__).parent / "features"
-
-for feature_file in features_path.rglob("*.feature"):
-    print(f"Loading feature: {feature_file}")
-    scenarios(str(feature_file))
