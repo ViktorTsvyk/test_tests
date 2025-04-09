89536ca5ec6f5ce06fe41ddd301405732c997b99
Victor Tsvyk <viktor.tsvyk@globallogic.com>
2025-04-09 17:32:45 +0300

    update sil uriy tests

--- sil/tests/sil_test_003/test_003.py
+++ sil/tests/sil_test_003/test_003.py
@@ -4,6 +4,7 @@ from lib.lib import load_images, verify_image
 
 from devsure.rpc.test.rpc_to_bpm_emu.tools import PinState
 from devsure.time_simulator import SimulatedTimeScope, SimulatedThread
+from devsure.formal import Step
 
 def test_measurement_nominal(tenv: TEnv) -> None:
     print("3 TEST STARTED!!!!! ==============================================================")
@@ -11,7 +12,9 @@ def test_measurement_nominal(tenv: TEnv) -> None:
     images = load_images()
 
     with SimulatedTimeScope(f"Test Case"):
-        SimulatedThread.sleep_ms(100)
+        with Step("Wait 100 ms for the device to initialize"):
+            SimulatedThread.sleep_ms(100)
+
         assert verify_image(tenv.emulator.screen_image, images.Home)
 
         tenv.emulator.click_button(PinState.ButtonOk)
@@ -26,20 +29,20 @@ def test_measurement_nominal(tenv: TEnv) -> None:
         tenv.emulator.click_button(PinState.ButtonOk)
         assert verify_image(tenv.emulator.screen_image, images.Measurement_InProgress, images.mask_measurement_progress)
 
-        #assert tenv.emulator.is_pump_enabled == True
-        #assert tenv.emulator.is_valve_enabled == False
+        assert tenv.emulator.is_pumping == True
+        assert tenv.emulator.is_valve_open == False
 
         SimulatedThread.sleep_ms(20000)
         assert verify_image(tenv.emulator.screen_image, images.Measurement_InProgress, images.mask_measurement_progress)
 
-        #assert tenv.emulator.is_pump_enabled == False
-        #assert tenv.emulator.is_valve_enabled == False
+        assert tenv.emulator.is_pumping == False
+        assert tenv.emulator.is_valve_open == False
 
         SimulatedThread.sleep_ms(50000)
         assert verify_image(tenv.emulator.screen_image, images.Measurement_Complete)
 
-        #assert tenv.emulator.is_pump_enabled == False
-        #assert tenv.emulator.is_valve_enabled == True
+        assert tenv.emulator.is_pumping == False
+        assert tenv.emulator.is_valve_open == True
 
     print("3 TEST ENDED !!!!! ==============================================================")
 
--- sil/tests/sil_test_004/test_004.py
+++ sil/tests/sil_test_004/test_004.py
@@ -4,13 +4,16 @@ from lib.lib import load_images, verify_image
 
 from devsure.rpc.test.rpc_to_bpm_emu.tools import PinState
 from devsure.time_simulator import SimulatedTimeScope, SimulatedThread
+from devsure.formal import Step
 
 def test_measurement_nominal(tenv: TEnv) -> None:
     print("4 TEST STARTED!!!!! ==============================================================")
     images = load_images()
 
     with SimulatedTimeScope(f"Test Case"):
-        SimulatedThread.sleep_ms(100)
+        with Step("Wait 100 ms for the device to initialize"):
+            SimulatedThread.sleep_ms(100)
+
         assert verify_image(tenv.emulator.screen_image, images.Home)
 
         tenv.emulator.click_button(PinState.ButtonOk)
@@ -27,19 +30,19 @@ def test_measurement_nominal(tenv: TEnv) -> None:
         tenv.emulator.click_button(PinState.ButtonOk)
         assert verify_image(tenv.emulator.screen_image, images.Measurement_InProgress, images.mask_measurement_progress)
 
-        #assert tenv.emulator.is_pump_enabled == True
-        #assert tenv.emulator.is_valve_enabled == False
+        assert tenv.emulator.is_pumping == True
+        assert tenv.emulator.is_valve_open == False
 
         SimulatedThread.sleep_ms(10000)
         # # it should still be pumping
         # assert verify_image(tenv.emulator.screen_image, images.Measurement_InProgress, images.mask_measurement_progress)
-        # #assert tenv.emulator.is_pump_enabled == True
-        # #assert tenv.emulator.is_valve_enabled == False
+        # assert tenv.emulator.is_pumping == True
+        # assert tenv.emulator.is_valve_open == False
 
         # SimulatedThread.sleep_ms(50000)
         assert verify_image(tenv.emulator.screen_image, images.Error_PreasureRemainsUnchanged)
 
-        #assert tenv.emulator.is_pump_enabled == False
-        #assert tenv.emulator.is_valve_enabled == True
+        assert tenv.emulator.is_pumping == False
+        assert tenv.emulator.is_valve_open == True
 
     print("4 TEST STARTED!!!!! ==============================================================")
